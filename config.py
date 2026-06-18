4f7ab3a46191e150e4ca9fcf37c21564ae6b19ea
Kailash Patel <kailash102004@gmail.com>
2026-06-19 00:07:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key-shop-insight'
-    # Default to PostgreSQL, but can be configured via .env
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:password@localhost:5432/shop_insight_hub'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///shopvision.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
